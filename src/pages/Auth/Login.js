2c848b3ba4dc8d643fbee068ce20d5c8f7e0aa27
zura-kuldeep <110804018+zura-kuldeep@users.noreply.github.com>
2022-08-08 17:50:46 +0530

    Update Login.js

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -4,15 +4,18 @@ import PropTypes from "prop-types";
 import classes from "./Login.module.css";
 import {Button, Container } from "reactstrap";
 
-// async function loginUser(credentials) {
-//   return fetch("http://localhost:8080/login", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(credentials),
-//   }).then((data) => data.json());
-// }
+async function loginUser(credentials) {
+  // return fetch("http://localhost:8080/login", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(credentials),
+  // }).then((data) => data.json());
+  return ({
+    "token": "test123"
+    })
+}
 
 const Login = ({ setToken }) => {
   const [username, setUser] = useState();
@@ -20,10 +23,10 @@ const Login = ({ setToken }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-//     const token = await loginUser({
-//       username,
-//       password,
-//     });
+    const token = await loginUser({
+      username,
+      password,
+    });
     setToken("232");
   };
 
