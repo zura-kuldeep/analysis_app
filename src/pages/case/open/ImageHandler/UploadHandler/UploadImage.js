49b8c8153a8d4321248f5ccec49f16577aca13f9
zura-kuldeep <110804018+zura-kuldeep@users.noreply.github.com>
2022-08-08 17:53:18 +0530

    Update UploadImage.js

--- src/pages/case/open/ImageHandler/UploadHandler/UploadImage.js
+++ src/pages/case/open/ImageHandler/UploadHandler/UploadImage.js
@@ -6,48 +6,51 @@ import CardRight from "../../../../../components/card/case/CardRight";
 
 const UploadImage = (props) => {
     const [images, setImage] = useState([]);
-    const [bytecode,setbytecode] = useState([]);
+    const [bytecode, setbytecode] = useState([]);
     const [view, setView] = useState();
     const [uploadButton, setUploadButton] = useState(false);
-    const [byte64,setByte64] = useState();
-    
+    const [byte64, setByte64] = useState();
 
     const fileSelectedHandler = (e) => {
+
         let val = e.target.files[0];
- 
-        function ree(val){
-        let reader =  new FileReader();
+
+
+        let reader = new FileReader();
         reader.readAsDataURL(val);
-        reader.onload=function(){
-          setByte64(reader.result);
-          let v = reader.result;
-          
-          return v;
+        reader.onload = function () {
+            setByte64(reader.result);
+            setdata(reader.result, val);
         }
-    }
-    console.log(ree(val));
 
 
-        let file = { byte64:byte64,file:e.target.files[0] }
-        setImage([file,...images]);
+        function setdata(byte64, val) {
+            let file = { byte64: byte64, file: val }
+            setImage([file, ...images]);
+        }
+
+
         setView(e.target.files[0]);
         setUploadButton(true);
-        console.log(file);
+
     };
+
+
     const deleteImage = (ima) => {
 
         let filteredImages = images;
-        
+
         filteredImages = filteredImages.filter(images => {
             return images.file.name !== ima.file.name;
         })
 
         setImage(filteredImages);
-        if(filteredImages.length>1){
-        setView(filteredImages[0].file);}
-        else if(filteredImages.length == 0){
-         setView();
-         setUploadButton(false);
+        if (filteredImages.length > 1) {
+            setView(filteredImages[0].file);
+        }
+        else if (filteredImages.length == 0) {
+            setView();
+            setUploadButton(false);
         }
     }
     const submithandler = () => {
@@ -66,6 +69,7 @@ const UploadImage = (props) => {
     return (<>
         <CardRight>
 
+        <form onSubmit={submithandler} style={{display:"flex"}}>
             <Input
             className={classes["file-input"]}
                 id="exampleFile"
@@ -73,39 +77,43 @@ const UploadImage = (props) => {
                 type="file"
                 onChange={fileSelectedHandler}
             />
-
-            <div style={{display:"flex"}}>
-            {view && <div className={classes["main-div"]}>
-                <div>
-                    <img className={classes["image-div"]} src={URL.createObjectURL(view)} alt="" />
-                </div>
-
-                <div>
-                    <div className={classes["row"]}>
-                        {images.map((images) => {
-                            return (
-                                <div className={classes["column"]}>
-                                    <img
-                                        className={classes["demo-img"]}
-                                        style={{ height: "99px", width: "99px" }}
-                                        src={URL.createObjectURL(images.file)}
-                                        key={"image uniqueid"}
-                                        alt=""
-                                        onClick={() => displayImage(images.file)}
-                                    />
-                                    <button className={classes["close-btn"]} onClick={() => deleteImage(images)}>x</button>
-                                </div>
-                            );
-                        })}
-                    </div>
-                </div>
-            </div>}
-            <div className={classes["upload-btn-div"]}>
+             <div className={classes["upload-btn-div"]}>
                 <Button onClick={cancelHandler} style={{ marginRight: "5px" }} outline>
                     cancel
                 </Button>
-                {uploadButton && <Button onClick={submithandler} outline>upload</Button>}
+                {uploadButton && <Button type="submit" outline>upload</Button>}
             </div>
+            </form>
+
+            
+
+            <div style={{ display: "flex" }}>
+                {view && <div className={classes["main-div"]}>
+                    <div>
+                        <img className={classes["image-div"]} src={URL.createObjectURL(view)} alt="" />
+                    </div>
+
+                    <div>
+                        <div className={classes["row"]}>
+                            {images.map((images) => {
+                                return (
+                                    <div className={classes["column"]}>
+                                        <img
+                                            className={classes["demo-img"]}
+                                            style={{ height: "99px", width: "99px" }}
+                                            src={URL.createObjectURL(images.file)}
+                                            key={"image uniqueid"}
+                                            alt=""
+                                            onClick={() => displayImage(images.file)}
+                                        />
+                                        <button className={classes["close-btn"]} onClick={() => deleteImage(images)}>x</button>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                </div>}
+               
             </div>
 
         </CardRight></>
