2f3d481443c97d81c047a888669bc93f2c72569a
zura-kuldeep <110804018+zura-kuldeep@users.noreply.github.com>
2022-08-25 16:55:27 +0530

    Update preViewVideo.js

--- src/pages/case/open/VideoHandler/DisplayVideo/preViewVideo.js
+++ src/pages/case/open/VideoHandler/DisplayVideo/preViewVideo.js
@@ -21,23 +21,18 @@ const PreViewVideo = (props) => {
 
         <div style={{ borderLeft: "1px solid black", marginLeft: "5px" }}></div>
         <div style={{ marginLeft: "20px", backgroundColor: "#ebeded", borderRadius: "10px",height:"300px", width:"550px" }}>
-        <div style={{ marginLeft: "20px" }}>
-          <p>File Name : {props.vidforProc.name}</p><br/>
-          <p>File Type : {props.vidforProc.type}</p><br/>
-          <p>File Size : {Math.floor(((props.vidforProc.size)/1024)/1024)}MB</p><br />
+        <div style={{ textAlign:"left",marginTop:"20px"}}>
+          <p style={{fontWeight:"600",marginLeft:"40px",fontSize:"18px"}}>File Name :<b>{props.vidforProc.name}                              </b> </p><br/>
+          <p style={{fontWeight:"600",marginLeft:"40px",fontSize:"18px"}}>File Type :<b>{props.vidforProc.type}                              </b> </p><br/>
+          <p style={{fontWeight:"600",marginLeft:"40px",fontSize:"18px"}}>File Size :<b>{Math.floor(((props.vidforProc.size)/1024)/1024)}MB  </b> </p><br />
         </div>
+        <div style={{marginTop:"50px",display:"flex",justifyContent:"center",alignItems:"center"}}>
+            <Button  outline style={{ marginRight: "10px", marginLeft: "5px",width:"150px" }}>Back</Button>
+            <Button  outline style={{width:"150px"}}>Proceed</Button>
+          </div>
         </div>
       </div>
 
-      <br></br>
-      <Button
-        onClick={displayvidsBack}
-        outline
-        style={{ marginRight: "10px", marginLeft: "10px" }}
-      >
-        Close
-      </Button>
-      <Button outline>Process</Button>
     </>
   );
 };
